5c8cf76f04f298ac1f85c0e9b522031062cb5550
Netanel Mazuz <netazuz@gmail.com>
2021-05-20 13:51:26 +0300

    first commit

---
+++ src/connection/socket.js
@@ -0,0 +1,17 @@
+import io from 'socket.io-client'
+
+const URL = 'https://multiplayer-chess-game-app.herokuapp.com/' 
+
+const socket = io(URL)
+
+var mySocketId
+
+socket.on("createNewGame", statusUpdate => {
+    console.log("A new game has been created! Username: " + statusUpdate.userName + ", Game id: " + statusUpdate.gameId + " Socket id: " + statusUpdate.mySocketId)
+    mySocketId = statusUpdate.mySocketId
+})
+
+export {
+    socket,
+    mySocketId
+}
